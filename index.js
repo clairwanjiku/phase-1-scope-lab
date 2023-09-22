478e876e578b89d2f328fe3f3f9c3107a516f1d9
Clair Wanjiku <clair.karanja@student.moringaschool.com>
2023-09-22 16:45:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob'; 
+
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = customerName.toUpperCase();
+  }
+}
+    
+
+    
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'; 
+  }
+changeLeastFavoriteCustomer 
+
+const leastFavoriteCustomer = 'someone'; 
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new value';
+  }
